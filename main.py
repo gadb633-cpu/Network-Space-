c47882b0b3e90ffb325a8afa6a6345046092ae6c
gad-biton <gadb633@gmail.com>
2026-07-26 11:01:04 +0300

    added level= 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from space_network_lib import SpaceEntity, Packet,SpaceNetwork
 class Satellite(SpaceEntity):
     def receive_signal(self, packet: Packet):
         print(f"[{self.name}]  Received: {packet}.")
-network = SpaceNetwork()
+network = SpaceNetwork(level=2)
 sat1 = Satellite("sat1",100)
 sat2 = Satellite("sat2",200)        
 massage = Packet("The satellite is excellent.",sat1,sat2)
